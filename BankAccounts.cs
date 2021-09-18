741626f505819dca4a557ed96a6e19e37fcc9b3b
Blest-1 <81591931+Blest-1@users.noreply.github.com>
2021-09-17 23:58:25 -0600

    Add files via upload

---
+++ BankAccounts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ATM_Final
+{
+    public class BankAccount
+    {
+        public string FullName { get; set; }
+        public Int64 AccountNumber { get; set; }
+        public Int64 CardNumber { get; set; }
+        public Int64 PinCode { get; set; }
+        public decimal Balance { get; set; }
+
+        public bool isLocked { get; set; }
+        private static BankAccount selectedAccount;
+        private static BankAccount inputAccount;
+
+        public void Initialization()
+        {
+            
+            {
+                new BankAccount() { FullName = "Matt", AccountNumber = 333111, CardNumber = 123, PinCode = 1111, Balance = 2000.00m, isLocked = false };
+                new BankAccount() { FullName = "Oscar", AccountNumber = 111222, CardNumber = 456, PinCode = 2222, Balance = 1500.30m, isLocked = true };
+                new BankAccount() { FullName = "Osiris", AccountNumber = 888555, CardNumber = 789, PinCode = 3333, Balance = 2900.12m, isLocked = false };
+            }
+        }
+    }
+}
